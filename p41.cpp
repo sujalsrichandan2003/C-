3467710b6c56b668fb3c72e907063cd893392b91
sujal <sujalsrichandan2003@gmail.com>
2023-10-10 22:50:23 +0530

    commit 41

---
+++ p41.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+	
+int main ()
+{
+	cout.width(5);
+	cout<<"s";
+	
+	cout.width(10);
+	cout<<"a";	
+}
+
+// The first cout.width(5) statement sets the width 5. the cout statement sets the colume width position at 5 and display the character 
+//"s" at column 5. similarly, the column width is set 10 and the character"a" is display at the 10th coloumn.
